887842ef61602b3479fc13b9a340de7b6e132ba2
Vigil Muriel Boniol <djibrilmuriel@gmail.com>
2020-08-12 01:30:03 +0800

    Initial Commit

---
+++ src/components/post/post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./post.styles.css";
+
+export const Post = () => {
+  return (
+    <div className="post">
+      <h3>Username</h3>
+      {/* header -> avatar + username */}
+      <img className="post_image" src="" alt="" />
+      {/* image */}
+      {/* username + caption */}
+    </div>
+  );
+};
+
+export default Post;
