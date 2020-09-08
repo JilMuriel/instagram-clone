32c611b5e71f5d1561b40dbe6000b4ae5950d957
Vigil Muriel Boniol <djibrilmuriel@gmail.com>
2020-09-08 21:59:20 +0800

    Add component for user stories item

---
+++ src/components/user-stories-item/user-stories-item.jsx
@@ -0,0 +1,10 @@
+import React from react;
+import './user-stories-item.styles.css'
+
+export const UserStoriesItem =() => {
+    return (
+        <h1>Hello from User Stories Item</h1>
+    )
+}
+
+export default UserStoriesItem;
