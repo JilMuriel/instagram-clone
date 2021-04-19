5f2bc8d33f592a3b0f01765356d6ad38322f0331
Jil Muriel <djibrilmuriel@gmail.com>
2021-04-19 17:41:57 +0800

    Add signup form

--- src/components/pages/sign-up/sign-up.jsx
+++ src/components/pages/sign-up/sign-up.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
+
 export const Signup = () => {
-  return <div>Hello from Sign-up</div>;
+  return (
+    <div className="sign-up">
+      <div className="sign-up__wrapper">
+        <div className="sign-up__card">
+          <div className="sign-up-card__header">
+            <span>
+              <h1>Instagram</h1>
+            </span>
+            <span>
+              <p>Sign up to see photos and videos from your friends.</p>
+            </span>
+            <button></button>
+          </div>
+          <div className="sign-up-card__body"></div>
+          <div className="sign-up-card__footer"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
