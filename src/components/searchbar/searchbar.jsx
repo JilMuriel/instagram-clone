9d0e5b9a58a37daa2dc33e392a12e2adfb9983f0
Jil Muriel <djibrilmuriel@gmail.com>
2021-07-20 10:29:51 +0800

    Update searchbar

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -4,14 +4,16 @@ import "./searchbar.styles.css";
 export const Searchbar = () => {
   const [placeHolder, setPlaceHolder] = useState(false);
   return (
-    <label className="searchbar">
-      <span className="searchbar__placeholder">
+    <label>
+      {/* <span className="searchbar__placeholder">
         <div className="searchbar-placeholder__wrapper">
           <span className="searchbar-cover__icon"></span>
           <span className="searchbar-cover__placeholder">Search</span>
         </div>
-      </span>
-      <input type="text" placeholder="Search" />
+      </span> */}
+      <span className="searchbar__icon"></span>
+      <input className="searchbar" type="text" placeholder="Search" />
+      <button className="searchbar__button"></button>
     </label>
   );
 };
